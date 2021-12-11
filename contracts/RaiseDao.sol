542e63913d3fcd90702eea89da4b5f7d898ce61e
Shrey Jain <31862368+shreyjain711@users.noreply.github.com>
2021-12-12 01:08:35 +0530

    Added more functionalities

--- contracts/RaiseDao.sol
+++ contracts/RaiseDao.sol
@@ -13,16 +13,21 @@ contract RaiseDao {
   uint constant votingPeriod = 2 weeks;
   uint constant disputablePeriod = 1 weeks;
   uint constant disputeCollateral = 100 ether;
-  
 
   struct Proposal {
-    uint64 amountToBeDebited;
+    string title
     string idea;
+    string ipfs;
+    string socialThread;
     address creator;
   }
 
   Proposal[] public proposals;
 
+  mapping (address => uint) voteBank;
+  mapping (uint => uint) proposalVotes;
+
+  event proposalCreated(address _from, uint _proposalId);
   event TransferReceived(address _from, uint _amount);
 
   constructor(
@@ -41,10 +46,38 @@ contract RaiseDao {
     _;
   }
 
+  modifier isMember () {
+    require(companyToken.balanceOf(msg.sender) > 0);
+    _;
+  }
+
   function applyForMembership() payable public membershipAvailable {
     require(msg.value <= yetToBeRaised, "Not enough membership tokens available");
     yetToBeRaised -= msg.value;
     companyToken.transfer(msg.sender, msg.value);
     emit TransferReceived(msg.sender, msg.value);
   }
+
+  function createProposal(string _title, string _idea, string _ipfs, string _socialThread) public returns (Proposal) {
+    Proposal memory newProp = new Proposal(_title, _idea, _ipfs, _socialThread, msg.sender));
+    proposals.push(newProp);
+    return newProp;
+  }
+
+  function getAllProposals() public view returns (Proposal[]) {
+    return proposals;
+  }
+
+  function getAllProposals() public view returns (uint) {
+    return proposals.length;
+  }
+
+  function voteForProposal(uint _proposalId, uint _lockedTokens) public payable returns (bool) {
+    
+  }
+
+  function disputeProposal(uint _proposalId, uint_collateral) public payable returns (bool) {
+
+  }
+
 }
