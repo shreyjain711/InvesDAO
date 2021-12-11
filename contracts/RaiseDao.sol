5794d53afd6e2561fd0fa04bfa111a4a1d5607a7
Shrey Jain <shrey.jain711@gmail.com>
2021-12-12 04:46:08 +0530

    core functionality done

--- contracts/RaiseDao.sol
+++ contracts/RaiseDao.sol
@@ -2,30 +2,39 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "./Token.sol";
+import "./Ownable.sol";
 
-contract RaiseDao {
+contract RaiseDao is Ownable {
   address invesDao;
-
   Token companyToken;
 
   uint raiseGoal;
   uint yetToBeRaised;
+  DAOStatus stageOfDAO;
   uint constant votingPeriod = 2 weeks;
   uint constant disputablePeriod = 1 weeks;
-  uint constant disputeCollateral = 100 ether;
+  uint constant minDisputeCollateral = 100 ether;
+
+  enum DAOStatus {VOTING, EXECUTING}
+  enum proposalStatus {ACTIVE, DISPUTED, ACCEPTED}
 
   struct Proposal {
-    string title
+    string title;
     string idea;
     string ipfs;
     string socialThread;
     address creator;
+    proposalStatus status;
+    address disputor;
   }
 
   Proposal[] public proposals;
 
   mapping (address => uint) voteBank;
-  mapping (uint => uint) proposalVotes;
+  address[] voters;
+  uint totalVoteTokens;
+
+  mapping (int => uint) proposalVotes;
 
   event proposalCreated(address _from, uint _proposalId);
   event TransferReceived(address _from, uint _amount);
@@ -39,6 +48,7 @@ contract RaiseDao {
     companyToken = new Token(_daoTokenName, _daoTokenSymbol, _raiseGoal);
     raiseGoal = _raiseGoal;
     yetToBeRaised = _raiseGoal;
+    stageOfDAO = DAOStatus.VOTING;
   }
 
   modifier membershipAvailable() {
@@ -51,33 +61,121 @@ contract RaiseDao {
     _;
   }
 
-  function applyForMembership() payable public membershipAvailable {
+  modifier currentlyRaising() {
+    require(stageOfDAO == DAOStatus.VOTING);
+    _;
+  }
+
+  modifier currentlyExecutiveBoard() {
+    require(stageOfDAO == DAOStatus.EXECUTING);
+    _;
+  }
+
+  function applyForMembership() payable public membershipAvailable currentlyRaising {
     require(msg.value <= yetToBeRaised, "Not enough membership tokens available");
     yetToBeRaised -= msg.value;
-    companyToken.transfer(msg.sender, msg.value);
+    companyToken.transfer(msg.sender, msg.value); 
     emit TransferReceived(msg.sender, msg.value);
   }
 
-  function createProposal(string _title, string _idea, string _ipfs, string _socialThread) public returns (Proposal) {
-    Proposal memory newProp = new Proposal(_title, _idea, _ipfs, _socialThread, msg.sender));
+  function createProposal(string memory _title, string memory _idea, string memory _ipfs, string memory _socialThread) public isMember currentlyRaising returns (bool) {
+    Proposal memory newProp = Proposal(_title, _idea, _ipfs, _socialThread, msg.sender, proposalStatus.ACTIVE, address(0));
     proposals.push(newProp);
-    return newProp;
+    return true;
   }
 
-  function getAllProposals() public view returns (Proposal[]) {
-    return proposals;
-  }
+  // ERR: cannot create dynamic size arrays
+  // function getAllProposals() public view returns (string[] memory, string[] memory, string[] memory, string[] memory, address[] memory, string[] memory) {
+  //   uint length = proposals.length;
+  //   string[] memory titles = string[length];
+  //   string[] memory ideas = string[length];
+  //   string[] memory ipfs = string[length];
+  //   string[] memory socialThreads = string[length];
+  //   address[] memory creators = address[length];
+  //   proposalStatus[] memory statuses = proposalStatus[length];
 
-  function getAllProposals() public view returns (uint) {
+  //   for(uint i = 0; i < proposals.length; i++){
+  //     titles.push(proposals[i].title);
+  //     ideas.push(proposals[i].idea);
+  //     ipfs.push(proposals[i].ipfs);
+  //     socialThreads.push(proposals[i].socialThread);
+  //     creators.push(proposals[i].creator);
+  //     statuses.push(proposals[i].status);
+  //   }
+
+  //   return (titles, ideas, ipfs, socialThreads, creators, statuses);
+  // }
+
+  function getNumOfProposals() public view returns (uint) {
     return proposals.length;
   }
 
-  function voteForProposal(uint _proposalId, uint _lockedTokens) public payable returns (bool) {
-    
+  function getProposalDetails(uint _proposalId) public view returns (string memory, string memory, string memory, string memory, address, proposalStatus, address) {
+    Proposal memory prop = proposals[_proposalId];
+    return (prop.title, prop.idea, prop.ipfs, prop.socialThread, prop.creator, prop.status, prop.disputor);
   }
 
-  function disputeProposal(uint _proposalId, uint_collateral) public payable returns (bool) {
-
+  function getVotingPower(uint _lockedTokens) internal returns (uint) {
+    if(_lockedTokens < 10) {
+      return 1;
+    } else if(_lockedTokens < 50) {
+      return 2;
+    } else if(_lockedTokens < 250) {
+      return 3;
+    } else if(_lockedTokens < 1000) {
+      return 4;
+    } else {
+      return 5;
+    }
   }
 
+  function voteForProposal(int _proposalId, uint _lockedTokens) public payable isMember currentlyRaising returns (bool) {
+    voteBank[msg.sender] += _lockedTokens;
+    proposalVotes[_proposalId] += getVotingPower(_lockedTokens);
+    return true;
+  }
+
+  function disputeProposal(uint _proposalId) public payable currentlyRaising returns (bool) {
+    require(proposals[_proposalId].status == proposalStatus.ACTIVE);
+    require(msg.value >= minDisputeCollateral);
+    proposals[_proposalId].status = proposalStatus.DISPUTED;
+    address payable sender = payable(msg.sender);
+    sender.transfer((msg.value - 1) * 1 ether);
+  }
+
+  // string memory, string memory, string memory, string memory, address memory, string memory
+  function pickTopProposal() internal onlyOwner currentlyRaising returns(int) {
+    int winnerPropsalId;
+    int maxSupport = -1;
+    for(int i = -1; i < int(proposals.length); i++) {
+      if(int(proposalVotes[i]) > maxSupport) {
+        maxSupport = int(proposalVotes[i]);
+        winnerPropsalId = i;
+      }
+    }
+    return winnerPropsalId;
+  }
+
+  function breakOrMake() external onlyOwner currentlyRaising returns(bool) {
+    if(totalVoteTokens < (raiseGoal / 4)){
+      disbandDAO();
+      return false;
+    } 
+    int topProposal = pickTopProposal();
+    if(topProposal == -1) {
+      disbandDAO();
+      return false;
+    }
+
+    makeBoardDAO();
+    return true;
+  }
+
+  function disbandDAO() internal onlyOwner currentlyRaising {
+    // TODO: send back MATIC as received from everyone
+  }
+
+  function makeBoardDAO() internal onlyOwner currentlyRaising {
+    // TODO: make the BoardDAO - send all MATIC to it, get back a token
+  }
 }
